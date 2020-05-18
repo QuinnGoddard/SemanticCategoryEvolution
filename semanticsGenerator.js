72c3bc5d138cc0a1354837ae1daa69c7f1a224dc
QuinnGoddard <qnn.gddrd@gmail.com>
2020-05-18 15:46:40 -0600

    Update semanticsGenerator.js

--- semanticsGenerator.js
+++ semanticsGenerator.js
@@ -1,1 +1,19 @@
+let appearedWords = new Set()
+const wordList = ["fer", "yaum", "xew", "pior", "trif", "drog"]
+
+
+// clicking on word adds properties of image to word
+function handleWordClick(event) {
+  // add word to list of words that have appeared
+  appearedWord.add(event.target.value)
+  
+}
+
+// each image associated with list of properties
+const images = {
+  A: {num: 1, color: blue, spots: yes, shape: square, file: ...},
+  B:
+  C:
+  D: 
+}
 
