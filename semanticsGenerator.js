61fbd1884899d224416a95f01c97131f4721eb05
QuinnGoddard <qnn.gddrd@gmail.com>
2020-05-20 09:58:07 -0600

    Update semanticsGenerator.js

--- semanticsGenerator.js
+++ semanticsGenerator.js
@@ -1,19 +1,38 @@
-let appearedWords = new Set()
-const wordList = ["fer", "yaum", "xew", "pior", "trif", "drog"]
+const wordList = {
+  "tiv": [0, 0, 0, 0, 0, 0, 0, 0],
+  "frooth": [0, 0, 0, 0, 0, 0, 0, 0],
+  "yaum": [0, 0, 0, 0, 0, 0, 0, 0],
+  "flig": [0, 0, 0, 0, 0, 0, 0, 0],
+  "rux": [0, 0, 0, 0, 0, 0, 0, 0],
+}
 
+/* each image associated with list of properties (1 for present, 0 for absent)
+      blue, black, red, spots, square, circle, round, jagged
+*/
+const images = {
+  A: [0, 1, 0, 0, 1, 0, 1, 0],
+  B: [1, 0, 0, 1, 0, 1, 0, 1]
+  C: [0, 0, 1, 0, 1, 0, 1, 1]
+  D: [1, 0, 0, 0, 0, 1, 1, 0]
+}
+
+const dict = {
+  "A": file,
+  "B": file,
+  "C": file,
+  "D": file
+}
 
 // clicking on word adds properties of image to word
-function handleWordClick(event) {
+function handleWordClick(event, image) {
   // add word to list of words that have appeared
-  appearedWord.add(event.target.value)
-  
+  let word = event.target.value
+  for (let i=0; i<8; i++) {
+      wordList[word][i]+=images[image][i] 
+  } 
 }
 
-// each image associated with list of properties
-const images = {
-  A: {num: 1, color: blue, spots: yes, shape: square, file: ...},
-  B:
-  C:
-  D: 
+function randImage() {
+  let random = dict.Keys.ElementAt((int).Random.Range(0, dict.Keys.Count - 1))
+  return imageList[random]
 }
-
