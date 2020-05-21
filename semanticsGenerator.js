6971a14e3d14ada59190587ab97d737ec82dfefb
QuinnGoddard <qnn.gddrd@gmail.com>
2020-05-20 20:19:38 -0600

    Update semanticsGenerator.js

--- semanticsGenerator.js
+++ semanticsGenerator.js
@@ -42,12 +42,12 @@ function handleEnd(event){
   let printly = ""
   const properties = ["blue", "black", "red", "spots", "square", "circle", "round", "jagged"]
   for (var key in wordList) {
-    printly += (key + "\n")
+    printly += (key + "\n\r")
     for (let i=0; i<properties.length; i++) {
       if (wordList[key][i] != 0) {
         printly += (properties[i] + wordList[key][i])
       }
-      printly += "/\n/"
+      printly += "\n\r"
     }
   }
   document.getElementById("results").innerHTML = printly
